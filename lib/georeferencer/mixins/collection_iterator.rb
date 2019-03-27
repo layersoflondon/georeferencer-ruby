beb9e9b0e8a2acaa5c48db8f877ec9d9d1791fb7
Ed Jones <ed@error.agency>
2019-03-27 18:30:39 +0000

    include `limit` param in iterator mixin

--- lib/georeferencer/mixins/collection_iterator.rb
+++ lib/georeferencer/mixins/collection_iterator.rb
@@ -11,12 +11,13 @@ module Georeferencer
 
         # check the params don't specifically include a :start argument (in which case we don't need to loop),
         # and that the response metadata includes a :start key
-        if !(@params.keys.include?(:start) || @params.keys.include?("start")) && @_collection.metadata[:start].present?
+        if !start_included? && @_collection.metadata[:start].present? && (!limited_included? || (limited_included? && @_collection.count < params[:limit]))
           @params.merge!(start: @_collection.metadata[:start])
           loop do
             clear_fetch_cache!
             new_collection = super
             @_collection += new_collection
+            break if limited_included? && @_collection.count >= @params[:limit]
             if new_collection.metadata[:start].present? && new_collection.metadata[:start] != @params[:start]
               @params.merge!(start: new_collection.metadata[:start])
             else
@@ -25,17 +26,23 @@ module Georeferencer
           end
         end
 
-        if @parent.preload_resources == true
-          Her::Collection.new(@_collection.collect(&:reload), metadata)
-        else
           Her::Collection.new(@_collection, metadata)
-        end
 
 
       else
         super
       end
     end
+
+    def limited_included?
+      (@params.keys.include?(:limit) || @params.keys.include?("limit"))
+    end
+
+    def start_included?
+      (@params.keys.include?(:start) || @params.keys.include?("start"))
+    end
+
+
   end
 
 end
